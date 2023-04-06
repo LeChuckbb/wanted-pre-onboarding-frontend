e6ab3f9b624e6d96fee10d26f540eef9453a8a9b
JaeWook Park <pjwts9412@gmail.com>
2023-04-06 13:34:11 +0900

    :sparkles: feat: 페이지 Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Todo from "./pages/Todo";
+
+const Main = () => {
+  return <div>Main</div>;
+};
+
+const routes = [
+  { path: "/", element: <Main /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/signin", element: <Signin /> },
+  { path: "/todo", element: <Todo /> },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {routes.map((route) => (
+          <li key={route.path}>
+            <Link to={route.path}>{route.path}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
     </div>
   );
 }
